6bda141b0f067d1ff39d9091d7c015cba5a3071f
HonsonCooky <HarrisonCook13@gmail.com>
2022-11-12 21:22:52 +1300

    feat: init project

---
+++ managing_the_projects/lib/common/presentation/mtp_app.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class MTPApp extends StatelessWidget {
+  const MTPApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+  
+}
